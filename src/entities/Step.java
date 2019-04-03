21c48156c70ab62762e2d48767bf35626ab8426c
Nenetti <akinori1090@gmail.com>
2019-04-03 20:50:07 +0900

    [add] TimeStepの全データも保持するように変更

--- src/entities/Step.java
+++ src/entities/Step.java
@@ -6,22 +6,35 @@ public class Step {
     private int[] xs;
     private int[] ys;
 
+    private int[] shortXs;
+    private int[] shortYs;
+
     private int resize = 20;
 
     public Step (int[] steps) {
         int length = steps.length / 2;
-        int cycle = length / resize;
-        int[] xs = new int[resize];
-        int[] ys = new int[resize];
-        for (int i = 0; i < resize; i++) {
-            xs[i] = steps[i * cycle * 2];
-            ys[i] = steps[i * cycle * 2 + 1];
+        int[] xs = new int[length];
+        int[] ys = new int[length];
+        for (int i = 0; i < length; i++) {
+            xs[i] = steps[i * 2];
+            ys[i] = steps[i * 2 + 1];
         }
-        xs[resize - 1] = steps[steps.length - 2];
-        ys[resize - 1] = steps[steps.length - 1];
         this.xs = xs;
         this.ys = ys;
+
+        int cycle = length / resize;
+        int[] shortXs = new int[resize];
+        int[] shortYs = new int[resize];
+        for (int i = 0; i < resize; i++) {
+            shortXs[i] = xs[i * cycle];
+            shortYs[i] = ys[i * cycle + 1];
+        }
+        shortXs[resize - 1] = xs[xs.length - 1];
+        shortYs[resize - 1] = ys[ys.length - 1];
+        this.shortXs = shortXs;
+        this.shortYs = shortYs;
     }
+
 
     public int[] getXs () {
         return xs;
@@ -29,5 +42,13 @@ public class Step {
 
     public int[] getYs () {
         return ys;
+    }
+
+    public int[] getShortXs () {
+        return shortXs;
+    }
+
+    public int[] getShortYs () {
+        return shortYs;
     }
 }
